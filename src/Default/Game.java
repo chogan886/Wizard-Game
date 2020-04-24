71612420d729299f992024abababfa3c01cfea40
chogan886 <chogan886@gmail.com>
2020-04-24 17:04:46 -0400

    Added level design and early camera

--- src/Default/Game.java
+++ src/Default/Game.java
@@ -2,6 +2,7 @@ package Default;
 
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 public class Game extends Canvas implements Runnable {
 
@@ -9,15 +10,22 @@ public class Game extends Canvas implements Runnable {
     private boolean isRunning = false;
     private Thread thread;
     private Handler handler;
+    private Camera camera;
+
+    private BufferedImage level = null;
 
     public Game(){
         new Window(1000, 563, "Wizard Game", this);
         start();
 
         handler = new Handler();
+        camera = new Camera (0, 0);
         this.addKeyListener(new KeyInput(handler));
 
-        handler.addObject(new Wizard(100, 100, ID.Player, handler));
+        BufferedImageLoader loader = new BufferedImageLoader();
+        level = loader.loadImage("/wizard_level.png");
+
+        loadLevel(level);
     }
 
     private void start(){
@@ -65,6 +73,13 @@ public class Game extends Canvas implements Runnable {
     }
 
     public void tick(){
+
+        for (int i = 0; i < handler.object.size(); i++) {
+            if(handler.object.get(i).getId() == ID.Player){
+                camera.tick(handler.object.get(i));
+            }
+        }
+
         handler.tick();
     }
 
@@ -76,16 +91,41 @@ public class Game extends Canvas implements Runnable {
         }
 
         Graphics g = bs.getDrawGraphics();
+        Graphics2D g2d = (Graphics2D) g;
         ///////////////////////////////////
 
         g.setColor(Color.red);
         g.fillRect(0, 0, 1000, 563);
 
+        g2d.translate(-camera.getX(), -camera.getY());
+
         handler.render(g);
 
+        g2d.translate(camera.getX(), camera.getY());
         ///////////////////////////////////
         g.dispose();
         bs.show();
+    }
+
+    // loading the level
+
+    private void loadLevel(BufferedImage image){
+        int w = image.getWidth();
+        int h = image.getHeight();
+
+        for (int xx = 0; xx < w; xx++) {
+            for (int yy = 0; yy < h; yy++) {
+                int pixel = image.getRGB(xx, yy);
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = (pixel) & 0xff;
+
+                if(red == 255)
+                    handler.addObject(new Block(xx*32, yy*32, ID.Block));
+                if(blue ==255)
+                    handler.addObject(new Wizard(xx*32, yy*32, ID.Player, handler));
+            }
+        }
     }
 
     public static void main(String[] args) {
